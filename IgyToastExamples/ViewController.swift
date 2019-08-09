e40a13190351eb5b5fb45e1450331a3538054c0e
Andrii Narinian <andrii.narinian@rolique.io>
2019-08-09 11:06:47 +0300

    initial commit

---
+++ IgyToastExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IgyToastExamples
+//
+//  Created by Andrii on 8/9/19.
+//  Copyright © 2019 io.igyvigy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
