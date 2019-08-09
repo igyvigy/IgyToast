3c21bc6b497180830a9004156c084ef31037ae4c
Andrii Narinian <andrii.narinian@rolique.io>
2019-08-09 10:57:55 +0300

    Initial Commit

---
+++ IgyToast/IgyToast.h
@@ -0,0 +1,19 @@
+//
+//  IgyToast.h
+//  IgyToast
+//
+//  Created by Andrii on 8/9/19.
+//  Copyright © 2019 io.igyvigy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IgyToast.
+FOUNDATION_EXPORT double IgyToastVersionNumber;
+
+//! Project version string for IgyToast.
+FOUNDATION_EXPORT const unsigned char IgyToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IgyToast/PublicHeader.h>
+
+
